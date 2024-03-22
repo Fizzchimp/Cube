510081a406e7601545b0685a46d57f5459618800
Fizzchimp <callumaskew@gmail.com>
2024-03-22 19:01:01 +0000

    changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame as pg
 
 class Cube():
     def __init__(self):
@@ -8,13 +8,13 @@ class Cube():
                       ["RR", "RR"],
                       ["GG", "GG"],
                       ["YY", "YY"]]
-pygame.init()
-screen = pygame.display.set_mode([500, 500])
+pg.init()
+screen = pg.display.set_mode([500, 500])
 screen.fill((255, 255, 255))
+pg.display.flip()
 
 running = True
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-pygame.quit()
+    for event in pg.event.get():
+        if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE): running = False
+pg.quit()
