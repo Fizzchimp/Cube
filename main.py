9a5ebb462d6d629d54b53956b5be64c33bc47287
Fizzchimp <callumaskew@gmail.com>
2024-03-23 01:29:02 +0000

    Started Movment

--- main.py
+++ main.py
@@ -2,12 +2,43 @@ import pygame as pg
 
 class Cube():
     def __init__(self):
-        self.cube1 = [["WW", "WW"],
-                      ["OO", "OO"],
-                      ["BB", "BB"],
-                      ["RR", "RR"],
-                      ["GG", "GG"],
-                      ["YY", "YY"]]
+        self.cube = [[["W", "W"], 
+                      ["W", "W"]],
+
+                     [["G", "G"], 
+                      ["G", "G"]],
+
+                     [["R", "R"], 
+                      ["R", "R"]],
+
+                     [["B", "B"], 
+                      ["B", "B"]],
+
+                     [["O", "O"], 
+                      ["O", "O"]],
+
+                     [["Y", "Y"], 
+                      ["Y", "Y"]]]
+        
+
+    def display(self):
+    def move_R(self, prime):
+        if prime:
+            sub1 = self.cube[0][1][1]
+            sub2 = self.cube[0][0][1]
+
+            self.cube[0][1][1] = self.cube[4][0][0]
+            self.cube[0][1][0] = self.cube[4][0][1]
+
+            self.cube[4][0][0] = self.cube[5][1][1]
+            self.cube[4][0][1] = self.cube[5][0][1]
+
+            self.cube[5][1][1] = self.cube[2][1][1]
+            self.cube[5][0][0] = self.cube[2][0][0]
+
+            self.cube[2][1][1] = sub1
+            self.cube[2][0][1] = sub2
+
 pg.init()
 screen = pg.display.set_mode([500, 500])
 screen.fill((255, 255, 255))
@@ -17,7 +48,9 @@ pg.display.set_icon(image)
 pg.display.set_caption("Cube")
 pg.display.flip()
 
-
+cube = Cube()
+cube.move_R(True)
+print(cube.cube)
 
 running = True
 while running:
