49ec81983a71a7f29f6dff9006efd2dd4ad5d8b7
Fizzchimp <callumaskew@gmail.com>
2024-03-29 14:09:42 +0000

    uhhhh

--- cube.py
+++ cube.py
@@ -1,3 +1,10 @@
+colours = {"W" : "\33[37m",
+           "Y" : "\33[33m",
+           "B" : "\33[34m",
+           "O" : "\033[93m",
+           "G" : "\33[32m",
+           "R" : "\33[31m"}
+
 class Cube():
     def __init__(self):
         # Representation of cube as an array
@@ -13,14 +20,18 @@ class Cube():
     def display(self):
         cube = self.cube
         for face in cube:
-            face.replace("W", "\33[37mW").replace("BBBB", "\33[34mBBBB")
+            newFace = ""
+            for x in face:
+                newFace += colours[x] + x
+            face = newFace
+            print(face)
         print(f"""
    |{cube[0][:2]}|
    |{cube[0][2:4]}|
 |{cube[1][:2]}|{cube[2][:2]}|{cube[3][:2]}|{cube[4][:2]}|
 |{cube[1][2:4]}|{cube[2][2:4]}|{cube[3][2:4]}|{cube[4][2:4]}|
    |{cube[5][:2]}|
-   |{cube[5][2:4]}|\33[37m""")
+   |{cube[5][2:4]}|\33[47m""")
    
     # Face rotation R
     def move_U(self, prime):
