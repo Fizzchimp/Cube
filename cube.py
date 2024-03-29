94282581b0e972ff7e37422ca82bce1c223feef3
Fizzchimp <callumaskew@gmail.com>
2024-03-29 14:25:41 +0000

    ugh

--- cube.py
+++ cube.py
@@ -1,10 +1,3 @@
-colours = {"W" : "\33[37m",
-           "Y" : "\33[33m",
-           "B" : "\33[34m",
-           "O" : "\033[93m",
-           "G" : "\33[32m",
-           "R" : "\33[31m"}
-
 class Cube():
     def __init__(self):
         # Representation of cube as an array
@@ -18,20 +11,13 @@ class Cube():
                      "YYYY"]
     # Display the cube in net form
     def display(self):
-        cube = self.cube
-        for face in cube:
-            newFace = ""
-            for x in face:
-                newFace += colours[x] + x
-            face = newFace
-            print(face)
         print(f"""
-   |{cube[0][:2]}|
-   |{cube[0][2:4]}|
-|{cube[1][:2]}|{cube[2][:2]}|{cube[3][:2]}|{cube[4][:2]}|
-|{cube[1][2:4]}|{cube[2][2:4]}|{cube[3][2:4]}|{cube[4][2:4]}|
-   |{cube[5][:2]}|
-   |{cube[5][2:4]}|\33[47m""")
+   |{self.cube[0][:2]}|
+   |{self.cube[0][2:4]}|
+|{self.cube[1][:2]}|{self.cube[2][:2]}|{self.cube[3][:2]}|{self.cube[4][:2]}|
+|{self.cube[1][2:4]}|{self.cube[2][2:4]}|{self.cube[3][2:4]}|{self.cube[4][2:4]}|
+   |{self.cube[5][:2]}|
+   |{self.cube[5][2:4]}|""")
    
     # Face rotation R
     def move_U(self, prime):
