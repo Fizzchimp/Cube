52c93272f40fe232ccb1cab6a795a8e1b12b454d
Fizzchimp <callumaskew@gmail.com>
2024-03-29 16:33:24 +0000

    Moement

--- cube.py
+++ cube.py
@@ -20,21 +20,30 @@ class Cube():
    |{self.cube[5][:2]}|
    |{self.cube[5][2:4]}|""")
    
-    # Face rotation U
-    def move_U(self, prime):
-        if not prime:
-            #U face rotation
-            self.cube[0] = self.cube[0][2] + self.cube[0][0] + self.cube[0][3] + self.cube[0][1]
+    # Rotation U clockwise
+    def U(self):
+        self.cube[0] = self.cube[0][2] + self.cube[0][0] + self.cube[0][3] + self.cube[0][1]
 
-            # Other faces rotation
-            sub = self.cube[1][:2]
-            self.cube[1] = self.cube[2][:2] + self.cube[1][2:4]
-            self.cube[2] = self.cube[3][:2] + self.cube[2][2:4]
-            self.cube[3] = self.cube[4][:2] + self.cube[3][2:4]
-            self.cube[4] = sub + self.cube[4][2:4]
+        sub = self.cube[1][:2]
+        for i in range(1, 4):
+            self.cube[i] = self.cube[i + 1][:2] + self.cube[i][2:4]
+        self.cube[4] = sub + self.cube[4][2:4]
 
-            
+    # Rotation U anticlockwise
+    def U_Prime(self):
+        self.cube[0] = self.cube[0][1] + self.cube[0][3] + self.cube[0][0] + self.cube[0][2]
+        
+        sub = self.cube[4][:2]
+        for i in range(3):
+            self.cube[4 - i] = self.cube[3 - i][:2] + self.cube[4 - i][2:4]
+        self.cube[1] = sub + self.cube[1][2:4]
+
+    # Rotation D clockwise
+    def D(self):
+        self.cube[5] = self.cube[5][2] + self.cube[5][0] + self.cube[0][3] + self.cube[0][1]
+        
 
 cube = Cube()
-cube.move_U(False)
+cube.U()
+cube.U_Prime()
 cube.display()
