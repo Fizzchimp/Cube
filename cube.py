e3fc1466c4589a565fc6cffa93a867d3a16b1176
Fizzchimp <callumaskew@gmail.com>
2024-03-29 14:38:53 +0000

    Made U rotation

--- cube.py
+++ cube.py
@@ -1,7 +1,7 @@
 class Cube():
     def __init__(self):
         # Representation of cube as an array
-        self.cube = ["ORGW",
+        self.cube = ["WWWW",
                      
                      "GGGG",
                      "RRRR",
@@ -9,6 +9,7 @@ class Cube():
                      "OOOO",
 
                      "YYYY"]
+        
     # Display the cube in net form
     def display(self):
         print(f"""
@@ -19,10 +20,18 @@ class Cube():
    |{self.cube[5][:2]}|
    |{self.cube[5][2:4]}|""")
    
-    # Face rotation R
+    # Face rotation U
     def move_U(self, prime):
         if not prime:
+            #U face rotation
             self.cube[0] = self.cube[0][2] + self.cube[0][0] + self.cube[0][3] + self.cube[0][1]
+
+            # Other faces rotation
+            sub = self.cube[1][:2]
+            self.cube[1] = self.cube[2][:2] + self.cube[1][2:4]
+            self.cube[2] = self.cube[3][:2] + self.cube[2][2:4]
+            self.cube[3] = self.cube[4][:2] + self.cube[3][2:4]
+            self.cube[4] = sub + self.cube[4][2:4]
 
             
 
