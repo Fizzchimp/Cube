31c6473d0949519873f676d2cee32b9b58300326
Fizzchimp <callumaskew@gmail.com>
2024-03-29 12:25:52 +0000

    Started Rotations

--- cube.py
+++ cube.py
@@ -1,7 +1,7 @@
 class Cube():
     def __init__(self):
         # Representation of cube as an array
-        self.cube = ["WWWW",
+        self.cube = ["BWOO",
                      
                      "GGGG",
                      "RRRR",
@@ -13,29 +13,21 @@ class Cube():
     def display(self):
         print(f"""
    |{self.cube[0][:2]}|
-   |{self.cube[0][2:4]}|
+   |{self.cube[0][3] + self.cube[0][2]}|
 |{self.cube[1][:2]}|{self.cube[2][:2]}|{self.cube[3][:2]}|{self.cube[4][:2]}|
 |{self.cube[1][2:4]}|{self.cube[2][2:4]}|{self.cube[3][2:4]}|{self.cube[4][2:4]}|
    |{self.cube[5][:2]}|
    |{self.cube[5][2:4]}|""")
    
     # Face rotation R
-    def move_R(self, prime):
-        if prime:
-            sub1 = self.cube[0][1][1]
-            sub2 = self.cube[0][0][1]
+    def move_U(self, prime):
+        if not prime:
+            self.cube[0] = self.cube[0][3] + self.cube[0][:3]
 
-            self.cube[0][1][1] = self.cube[4][0][0]
-            self.cube[0][1][0] = self.cube[4][0][1]
-
-            self.cube[4][0][0] = self.cube[5][1][1]
-            self.cube[4][0][1] = self.cube[5][0][1]
-
-            self.cube[5][1][1] = self.cube[2][1][1]
-            self.cube[5][0][0] = self.cube[2][0][0]
-
-            self.cube[2][1][1] = sub1
-            self.cube[2][0][1] = sub2
+            
 
 cube = Cube()
+# cube.display()
+cube.move_U(False)
+cube.move_U(False)
 cube.display()
