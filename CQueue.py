48db028f6a0e0a8c8a1a0b4679d1d7a7b785dd8c
Callum Askew <154433436+Fizzchimp@users.noreply.github.com>
2024-04-11 12:29:37 +0100

    Update CQueue.py

--- CQueue.py
+++ CQueue.py
@@ -1,50 +1,47 @@
 class Queue:
-    def __init__(self, size):
-        self.size = size
-        self.front = 0
-        self.rear = -1
-        
-        self.queue = [None for i in range(size)]
+  def __init__(self, size):
+      self.size = size
+      self.front = 0
+      self.rear = -1
+
+      self.queue = [None for i in range(size)]
 
 
-    def isFull(self):
-        if self.rear == self.front + self.size - 1:
-            return True
-        return False
-        
-    def isEmpty(self):
-        if self.rear == self.front - 1:
-            return True
-        return False
+  def isFull(self):
+      if self.rear == self.front + self.size - 1:
+          return True
+      return False
+
+  def isEmpty(self):
+      if self.rear == self.front - 1:
+          return True
+      return False
 
 
-    def enqueue(self, item):
-        if not self.isFull():
-            self.rear += 1
-            self.queue[self.rear] = item
-        else: print("Queue is full! Cannot enqueue")
-     
-    def dequeue(self):
-        if not self.isEmpty():
-            self.front += 1
-            return self.queue[self.front - 1]
-        else: print("Queue is empty! Cannot dequeue")
-    
+  def enqueue(self, item):
+      if not self.isFull():
+          self.rear += 1
+          self.queue[self.rear % self.size] = item
+      else:
+        print("Queue is full! Cannot enqueue")
 
-    def display(self):
-        print("[", end = "")
-        for i in range(self.front, self.rear):
-            print(self.queue[i], end = ", ")
-        print(str(self.queue[self.rear]) + "]")
-        print("Front:", self.queue[self.front], "\nRear:", self.queue[self.rear])
-        print("Items:", self.rear - self.front + 1)
+  def dequeue(self):
+      if not self.isEmpty():
+          self.front += 1
+          return self.queue[(self.front - 1) % self.size]
+      else:
+        print("Queue is empty! Cannot dequeue")
 
 
-q = Queue(6)
+  def display(self):
+      front, rear = self.front % self.size, self.rear % self.size
 
-for i in range(7):
-    q.enqueue(10 * i)
+      print("[", end = "")
+      for i in range(self.front, self.rear):
+          print(self.queue[i % self.size], end = ", ")
+      print(str(self.queue[rear]) + "]")
 
-q.display()
-print(q.dequeue())
-q.display()
+      print("Front:", self.queue[front])
+      print("Rear:", self.queue[rear])
+      print("Items:", self.rear - self.front + 1)
+      print("Spaces:", self.size - (self.rear - self.front + 1))
