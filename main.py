5fab842cf8c027cad119842d8346f84111ecc152
Fizzchimp <callumaskew@gmail.com>
2024-04-12 21:24:52 +0100

    Updated

--- main.py
+++ main.py
@@ -2,14 +2,9 @@ import pygame as pg
 from display import Display
 from cube import Cube
 
-
-screen = Display(700, 700)
-
-running = True
-while running:
-    for event in pg.event.get():
-        if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE): running = False
+class World:
+    def __init__(self):
+        window = Display(700, 700)
 
 
-    screen.draw_cube()
-pg.quit()
+world = World()
