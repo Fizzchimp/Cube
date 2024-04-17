db53e42c325b399fcdbb483d8663fa7a29e9839d
Fizzchimp <callumaskew@gmail.com>
2024-04-17 18:28:38 +0100

    Changed methods to be non-mutable

--- cube.py
+++ cube.py
@@ -54,22 +54,26 @@ class Cube():
                 self.cube[5]]
 
     def D(self):
-        # Rotation D clockwise
-        self.cube[5] = self.cube[5][2] + self.cube[5][0] + self.cube[5][3] + self.cube[5][1]
-
-        sub = self.cube[4][2:4]
-        for i in range(3):
-            self.cube[4 - i] = self.cube[4 - i][:2] + self.cube[3 - i][2:4]
-        self.cube[1] = self.cube[1][:2] + sub
+        # Returns rotation D clockwise
+        return [self.cube[0],
+                
+                self.cube[1][2:] + self.cube[4][2:4],
+                self.cube[2][:2] + self.cube[1][2:4],
+                self.cube[3][:2] + self.cube[2][2:4],
+                self.cube[4][:2] + self.cube[3][2:4],
+                
+                self.cube[5][2] + self.cube[5][0] + self.cube[5][3] + self.cube[5][1]]
 
     def D_Prime(self):
-        # Rotation D anticlockwise
-        self.cube[5] = self.cube[5][1] + self.cube[5][3] + self.cube[5][0] + self.cube[5][2]
+        # Returns rotation D anticlockwise
+        return [self.cube[0],
+                
+                self.cube[1][:2] + self.cube[2][2:4],
+                self.cube[2][:2] + self.cube[3][2:4],
+                self.cube[3][:2] + self.cube[4][2:4],
+                self.cube[4][:2] + self.cube[1][2:4],
 
-        sub = self.cube[1][2:4]
-        for i in range(1, 4):
-            self.cube[i] = self.cube[i][:2] + self.cube[i + 1][2:4]
-        self.cube[4] = self.cube[4][:2] + sub
+                self.cube[5][1] + self.cube[5][3] + self.cube[5][0] + self.cube[5][2]]
 
 
     def F(self):
@@ -95,24 +99,26 @@ class Cube():
                 self.cube[1][1] + self.cube[1][3] + self.cube[5][2:4]]
 
     def B(self):
-        # Rotation B clockwise
-        self.cube[4] = self.cube[4][2] + self.cube[4][0] + self.cube[4][3] + self.cube[4][1]
+        # Returns rotation B clockwise
+        return [self.cube[3][1] + self.cube[3][3] + self.cube[0][2:4],
+                
+                self.cube[0][0] + self.cube[1][1] + self.cube[0][1] + self.cube[1][3],
+                self.cube[2],
+                self.cube[3][0] + self.cube[5][3] + self.cube[3][2] + self.cube[5][2],
+                self.cube[4][2] + self.cube[4][0] + self.cube[4][3] + self.cube[4][1],
 
-        sub = self.cube[0][:2]
-        self.cube[0] = self.cube[3][1] + self.cube[3][3] + self.cube[0][2:4]
-        self.cube[3] = self.cube[3][0] + self.cube[5][3] + self.cube[3][2] + self.cube[5][2]
-        self.cube[5] = self.cube[5][:2] + self.cube[1][0] + self.cube[1][2]
-        self.cube[1] = sub[1] + self.cube[1][1] + sub[0] + self.cube[1][3]
+                self.cube[5][:2] + self.cube[1][0] + self.cube[1][2]]
     
     def B_Prime(self):
-        # Rotatio B anticlockwise
-        self.cube[4] = self.cube[4][1] + self.cube[4][3] + self.cube[4][0] + self.cube[4][2]
-        
-        sub = self.cube[0][:2]
-        self.cube[0] = self.cube[1][2] + self.cube[1][0] + self.cube[0][2:4]
-        self.cube[1] = self.cube[5][2] + self.cube[1][1] + self.cube[5][3] + self.cube[1][3]
-        self.cube[5] = self.cube[5][:2] + self.cube[3][1] + self.cube[3][3]
-        self.cube[3] = self.cube[3][0] + sub[0] + self.cube[3][2] + sub[1]
+        # Returns rotation B anticlockwise
+        return [self.cube[1][2] + self.cube[1][0] + self.cube[0][2:4],
+                
+                self.cube[5][2] + self.cube[1][1] + self.cube[5][3] + self.cube[1][3],
+                self.cube[2],
+                self.cube[3][0] + self.cube[0][0] + self.cube[3][2] + self.cube[0][1],
+                self.cube[4][1] + self.cube[4][3] + self.cube[4][0] + self.cube[4][2],
+
+                self.cube[5][:2] + self.cube[3][1] + self.cube[3][3]]
 
 
     def L(self):
@@ -159,4 +165,3 @@ class Cube():
                 self.cube[5][3] + self.cube[4][1] + self.cube[5][1] + self.cube[4][3],
 
                 self.cube[5][0] + self.cube[2][1] + self.cube[5][2] + self.cube[2][3]]
-    
