4c711d7745637e5f544c0e65562ae9f95cda5c98
Fizzchimp <callumaskew@gmail.com>
2024-04-23 20:14:10 +0100

    Updated Cqueue

--- binSearch.py
+++ binSearch.py
@@ -1,23 +1,26 @@
-def binSearch(arr, item, front = 0, rear = None):
-    if rear == None: rear = len(arr) - 1
-    mid = (rear + front) // 2
-    midItem = arr[mid]
-    
-    if midItem == item:
-        print(f"Found at pos {mid}")
-        return
-    
-    elif front == rear:
-        print("Not found. Inserting.")
-        #if midItem > item: list.insert(mid, item)
-        arr.insert(mid if midItem > item else mid + 1, item)
-        return
-    
-    elif midItem < item:
-        return binSearch(arr, item, mid + 1, rear)
-    
-    elif midItem > item:
-        return binSearch(arr, item, 0, mid - 1)
+class ordList:
+    def __init__(self):
+        self.list = []
+    def binSearch(self, arr, item, front = 0, rear = None):
+        if rear == None: rear = len(arr) - 1
+        mid = (rear + front) // 2
+        midItem = arr[mid]
+        
+        if midItem == item:
+            print(f"Found at pos {mid}")
+            return
+        
+        elif front == rear:
+            print("Not found. Inserting.")
+            #if midItem > item: list.insert(mid, item)
+            arr.insert(mid if midItem > item else mid + 1, item)
+            return
+        
+        elif midItem < item:
+            return binSearch(arr, item, mid + 1, rear)
+        
+        elif midItem > item:
+            return binSearch(arr, item, 0, mid - 1)
 
 
 arr1 = [0]
