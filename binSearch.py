1173358c37b8a0ea4a944b615169364bd0b5a535
18askewc <18askewc@staidans.co.uk>
2024-04-24 12:25:38 +0100

    Updated BinSearch

--- binSearch.py
+++ binSearch.py
@@ -1,30 +1,41 @@
-class ordList:
-    def __init__(self):
-        self.list = []
-    def binSearch(self, arr, item, front = 0, rear = None):
-        if rear == None: rear = len(arr) - 1
-        mid = (rear + front) // 2
-        midItem = arr[mid]
-        
-        if midItem == item:
-            print(f"Found at pos {mid}")
-            return
-        
-        elif front == rear:
-            print("Not found. Inserting.")
-            #if midItem > item: list.insert(mid, item)
-            arr.insert(mid if midItem > item else mid + 1, item)
-            return
-        
-        elif midItem < item:
-            return binSearch(arr, item, mid + 1, rear)
-        
-        elif midItem > item:
-            return binSearch(arr, item, 0, mid - 1)
+#def binSearch(arr, item, front = 0, rear = None):
+#    if rear == None: rear = len(arr) - 1
+#    mid = (rear + front) // 2
+#    midItem = arr[mid]
+#        
+#    if midItem == item:
+#        print(f"Found at pos {mid}")
+#        return
+#        
+#    elif front == rear:
+#        print("Not found. Inserting.")
+#        arr.insert(mid if midItem > item else mid + 1, item)
+#        return
+#        
+#    elif midItem < item:
+#        return binSearch(arr, item, mid + 1, rear)
+#        
+#    elif midItem > item:
+#        return binSearch(arr, item, 0, mid - 1)
 
 
-arr1 = [0]
+def binSearch(arr, item):
+    mid = len(arr) // 2
+    print(mid)
+    midItem = arr[mid]
+        
+    if midItem == item:
+        return True
+        
+    elif len(arr) == 1:
+        return False
+        
+    elif midItem < item:
+        return binSearch(arr[:mid + 1], item)
+        
+    elif midItem > item:
+        return binSearch(arr[mid:], item)
 
-for i in range(1000):
-    binSearch(arr1, 1000 - i)
-print(arr1)
+
+arr1 = [i for i in range(10)]
+print()
