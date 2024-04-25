13ab37ff8612cabee5b9250337735e3ed304548e
18askewc <18askewc@staidans.co.uk>
2024-04-25 13:23:05 +0100

    Added mergeSort

---
+++ mergeSort.py
@@ -0,0 +1,41 @@
+import random
+
+def mergeSort(list):
+  print(list)
+  if len(list) > 1:
+    left = list[:len(list) // 2]
+    right = list[len(list) // 2:]
+    mergeSort(left)
+    mergeSort(right)
+  
+    i = j = k = 0
+    while i < len(left) and j < len(right):
+      if left[i] < right[j]:
+        list[k] = left[i]
+        i += 1
+      else:
+        list[k] = right[j]
+        j += 1
+      k += 1
+    
+    while i < len(left):
+      list[k] = left[i]
+      k += 1
+      i += 1
+    while j < len(left):
+      list[k] = right[j]
+      k += 1
+      j += 1
+  return list
+
+def mergeSort(list):
+  if len(list) > 1:
+    left = list[:len(list) // 2]
+    right = list[len(list) // 2:]
+    print(left, right)
+  
+arr = [random.randint(1, 100) for i in range(15)]
+print(arr)
+
+
+print(mergeSort(arr))
