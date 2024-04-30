5b3687ac041adac745234abfb6bd3a8584d389e8
Callum Askew <154433436+Fizzchimp@users.noreply.github.com>
2024-04-30 13:15:35 +0100

    Update binSearch.py

--- binSearch.py
+++ binSearch.py
@@ -1,16 +1,25 @@
+# Recursive binary Searching algorithm
 def binSearch(arr, item, front = 0, rear = None):
-    if rear == None: rear = len(arr) - 1
+    if rear == None:
+        rear = len(arr) - 1
+    if rear == -1: 
+        return False
     mid = (rear + front) // 2
     midItem = arr[mid]
-        
-    if midItem == item:
+    
+    # Check if the middle item is the search term
+    if midItem.cube == item.cube:
         return True
-        
+    
+    # Check if the range is 0 (item not in list)
     elif front == rear:
         return False
         
-    elif midItem < item:
+    # Check if the middle item is larger than the search term
+    elif midItem.cube < item.cube:
         return binSearch(arr, item, mid + 1, rear)
         
-    elif midItem > item:
-        return binSearch(arr, item, 0, mid - 1)
+    # Check if the middle item is smaller than the search term
+    elif midItem.cube > item.cube:
+        return binSearch(arr, item, front, mid - 1)
+    
