95b3d3f9e3077dfe14648bd479f0eb959fc064b8
Callum Askew <154433436+Fizzchimp@users.noreply.github.com>
2024-04-30 13:19:20 +0100

    Update node.py

--- node.py
+++ node.py
@@ -1,5 +1,11 @@
-class Node:
-    def __init__(self, cube, parent = None, move = None):
-        self.cube = cube
+from cube import Cube
+
+class Node(Cube):
+    def __init__(self, cube = None, parent = None, movement = None, generation = 0):
+        Cube.__init__(self, cube)
         self.parent = parent
-        self.move = move
+        self.movement = movement
+        self.generation = generation
+
+    # def clone(self):
+    #     return Node(self.cube, self.parent, self.movement, self.generation)
