3fdbb6afceba564bfd45ca773195015f65f9a32e
18askewc <18askewc@staidans.co.uk>
2024-05-10 11:09:38 +0100

    Updated display

--- display.py
+++ display.py
@@ -1,50 +1,31 @@
 import pygame as pg
 import numpy as np
-COS30 = np.cos(np.pi / 6)
-THRDPI = np.pi / 3
 angle = 0
 
-class CubePoints:
-    def __init__(self, length):
-        # self.points = [
-        #     [length, length, length],
-        #     [length, length, -length],
-        #     [-length, length, -length],
-        #     [-length, length, length],
-
-        #     [length, -length, length],
-        #     [length, -length, -length],
-        #     [-length, -length, -length],
-        #     [-length, -length, length]]
+       
+def rotateX(angle, points):
+    angle = angle / 180 * np.pi
+    rotX = [[1, 0, 0],
+            [0, np.cos(angle), -np.sin(angle)],
+            [0, np.sin(angle), np.cos(angle)]]
         
-        self.points = [
-            [length, length, -length, -length, length, length, -length, -length],
-            [length, length, length, length, -length, -length, -length, -length],
-            [length, -length, -length, length, length, -length, -length, length]]
+    return np.matmul(rotX, points)
         
-    def rotateX(self, angle):
-        angle = angle / 180 * np.pi
-        rotX = [[1, 0, 0],
-                [0, np.cos(angle), -np.sin(angle)],
-                [0, np.sin(angle), np.cos(angle)]]
-        return np.matmul(rotX, self.points)
+def rotateY(angle, points):
+    angle = angle / 180 * np.pi
+    rotY = [[np.cos(angle), 0, np.sin(angle)],
+            [0, 1, 0],
+            [-np.sin(angle), 0, np.cos(angle)]]
         
-    def rotateY(self, angle):
-        angle = angle / 180 * np.pi
-        rotY = [[np.cos(angle), 0, np.sin(angle)],
-                [0, 1, 0],
-                [-np.sin(angle), 0, np.cos(angle)]]
-        
-        return np.matmul(rotY, self.points)
+    return np.matmul(rotY, points)
     
-    def rotateZ(self, angle):
-        angle = angle / 180 * np.pi
-        rotZ = [[np.cos(angle), -np.sin(angle), 0],
-                [np.sin(angle), np.cos(angle), 0],
-                [0, 0, 1]]
+def rotateZ(angle, points):
+    angle = angle / 180 * np.pi
+    rotZ = [[np.cos(angle), -np.sin(angle), 0],
+            [np.sin(angle), np.cos(angle), 0],
+            [0, 0, 1]]
         
-        return np.matmul(rotZ, self.points)
-    
+    return np.matmul(rotZ, points)
         
 
         
@@ -63,24 +44,46 @@ class Display():
         self.x, self.y  = self.width / 2, self.height / 2
 
         # 3D Matrix of all verticies in a cube
-        self.length = self.width / 5 if self.width <= self.height else self.height / 5
-        self.points = CubePoints(self.length)
+        length = self.width / 5 if self.width <= self.height else self.height / 5
+        self.cubePoints = [
+            [length, length, -length, -length, length, length, -length, -length],
+            [length, length, length, length, -length, -length, -length, -length],
+            [length, -length, -length, length, length, -length, -length, length]]
+
+        self.yPhase = 0
+        self.xPhase = 0
+        
 
     def draw_cube(self):
+        points = rotateX(30 + self.xPhase, rotateY(20 + self.yPhase, self.cubePoints))
         self.screen.fill((255, 255, 255))
-
-        # for i in range(4):
-        #     pg.draw.line(
-        #         self.screen,
-        #         (100, 100, 100),
-        #         (self.x + self.points.points[i][0], self.y + self.points.points[i][1]),
-        #         (self.x + self.points.points[i + 4][0], self.y + self.points.points[i + 4][1]))
             
         for i in range(8):
-            pg.draw.circle(self.screen, (150 + 0.25 * self.points.points[2][i], 100, 100), (self.x + self.points.points[0][i], self.y + self.points.points[1][i]), 4)
-        self.points.points = self.points.rotateY(0.25)
-        self.points.points = self.points.rotateZ(0.2)
-        self.points.points = self.points.rotateX(0.1)
+            pg.draw.circle(self.screen, (150 + -0.25 * points[2][i], 100, 100), (self.x + points[0][i], self.y + points[1][i]), 4)
+
+
+        for i in range(4):
+            pg.draw.line(self.screen,
+                         (100, 100, 100),
+                         (self.x + points[0][i], self.y + points[1][i]),
+                         (self.x + points[0][4 + i], self.y + points[1][i + 4]))
+            
+            pg.draw.line(self.screen,
+                         (100, 100, 100),
+                         (self.x + points[0][i], self.y + points[1][i]),
+                         (self.x + points[0][(i + 1) % 4], self.y + points[1][(i + 1) % 4]))
+            
+            pg.draw.line(self.screen,
+                         (100, 100, 100),
+                         (self.x + points[0][i + 4], self.y + points[1][i + 4]),
+                         (self.x + points[0][(i + 1) % 4 + 4], self.y + points[1][(i + 1) % 4 + 4]))
+            
+
+
+        if self.yPhase < 0: self.yPhase += 1
+        if self.yPhase > 0: self.yPhase -= 1
+        if self.xPhase < 0: self.xPhase += 1
+        if self.xPhase > 0: self.xPhase -= 1
 
 def main():
     screen = Display(700, 700)
@@ -94,14 +97,15 @@ def main():
 
         if keyDown:
             if key == pg.K_ESCAPE: running = False
-        #     if key == pg.K_RIGHT and screen.yPhase == 0: screen.yPhase = -90
-        #     if key == pg.K_LEFT and screen.yPhase == 0: screen.yPhase = 90
-        #     if key == pg.K_DOWN and screen.xPhase == 0: screen.xPhase = -90
-        #     if key == pg.K_UP and screen.xPhase == 0: screen.xPhase = 90
+            #if key == pg.K_SPACE: screen.screen.fill((255, 255, 255))
+            if key == pg.K_RIGHT and screen.yPhase == 0: screen.yPhase = 90
+            if key == pg.K_LEFT and screen.yPhase == 0: screen.yPhase = -90
+            if key == pg.K_DOWN and screen.xPhase == 0: screen.xPhase = -90
+            if key == pg.K_UP and screen.xPhase == 0: screen.xPhase = 90
 
         screen.draw_cube()
         pg.display.flip()
-        pg.time.wait(5)
+        pg.time.wait(3)
         
 main()
 pg.quit()
--- test.py
+++ test.py
@@ -1,17 +1,47 @@
 import numpy as np
-
-
-
-angle = np.pi / 2
-rotY = np.array([[np.cos(angle), 0, np.sin(angle)],
-                 [0, 1, 0],
-                 [-np.sin(angle), 0, np.cos(angle)]])
-
 length = 1
-points = [
-    [length, length, -length, -length, length, length, -length, -length],
-    [length, length, length, length, -length, -length, -length, -length],
-    [length, -length, -length, length, length, -length, -length, length]]
-        
 
-print(np.matmul(rotY, points))
+def rotateTest(theta, alpha, points):
+    theta, alpha = theta / 180 * np.pi, alpha / 180 * np.pi
+    rotTest = [[np.cos(theta), np.sin(theta) * np.sin(alpha), -np.sin(theta) * np.cos(alpha)],
+               [0, np.cos(alpha), -np.sin(alpha)],
+               [-np.sin(theta), np.sin(theta) * np.sin(alpha), -np.cos(theta) * np.cos(alpha)]]
+    
+    return np.matmul(rotTest, points)
+
+def rotateX(angle, points):
+    angle = angle / 180 * np.pi
+    rotX = [[1, 0, 0],
+            [0, np.cos(angle), -np.sin(angle)],
+            [0, np.sin(angle), np.cos(angle)]]
+        
+    return np.matmul(rotX, points)
+        
+def rotateY(angle, points):
+    angle = angle / 180 * np.pi
+    rotY = [[np.cos(angle), 0, np.sin(angle)],
+            [0, 1, 0],
+            [-np.sin(angle), 0, np.cos(angle)]]
+        
+    return np.matmul(rotY, points)
+    
+def rotateZ(angle, points):
+    angle = angle / 180 * np.pi
+    rotZ = [[np.cos(angle), -np.sin(angle), 0],
+            [np.sin(angle), np.cos(angle), 0],
+            [0, 0, 1]]
+        
+    return np.matmul(rotZ, points)
+
+cubePoints = [
+            [length, length, -length, -length, length, length, -length, -length],
+            [length, length, length, length, -length, -length, -length, -length],
+            [length, -length, -length, length, length, -length, -length, length]]
+
+newPoints = rotateTest(90, 90, cubePoints)
+newPoints1 = rotateX(90, rotateY(90, cubePoints))
+
+for i in range(8):
+    print(newPoints[0][i], newPoints[1][i], newPoints[2][i])
+    print(newPoints1[0][i], newPoints1[1][i], newPoints1[2][i])
+    print("---------------")
