05a624714388888f7dd4391a46d7ba77ceb95614
Fizzchimp <callumaskew@gmail.com>
2024-05-11 23:13:53 +0100

    Updated

--- display.py
+++ display.py
@@ -4,34 +4,52 @@ angle = 0
        
 def rotateX(angle, points):
     angle = angle / 180 * np.pi
-    rotX = [[1, 0, 0],
-            [0, np.cos(angle), -np.sin(angle)],
-            [0, np.sin(angle), np.cos(angle)]]
+    axis = [0.93969262, 0.17101007, -0.29619813]
+    rotX = [[np.cos(angle) + axis[0] * axis[0] * (1 - np.cos(angle)),
+            axis[0] * axis[1] * (1 - np.cos(angle)) - axis[2] * np.sin(angle),
+            axis[0] * axis[2] * (1 - np.cos(angle)) + axis[1] * np.sin(angle)],
+
+            [axis[0] * axis[1] * (1 - np.cos(angle)) + axis[2] * np.sin(angle),
+            np.cos(angle) + axis[1] * axis[1] * (1 - np.cos(angle)),
+            axis[1] * axis[2] * (1 - np.cos(angle)) - axis[0] * np.sin(angle)],
+
+            [axis[0] * axis[2] * (1 - np.cos(angle)) - axis[1] * np.sin(angle),
+            axis[1] * axis[2] * (1 - np.cos(angle)) + axis[0] * np.sin(angle),
+            np.cos(angle) + axis[2] * axis[2] * (1 - np.cos(angle))]]
         
     return np.matmul(rotX, points)
         
 def rotateY(angle, points):
     angle = angle / 180 * np.pi
-    rotY = [[np.cos(angle), 0, np.sin(angle)],
-            [0, 1, 0],
-            [-np.sin(angle), 0, np.cos(angle)]]
+    axis = [0, 0.8660254, 0.5]
+    rotY = [[np.cos(angle) + axis[0] * axis[0] * (1 - np.cos(angle)),
+            axis[0] * axis[1] * (1 - np.cos(angle)) - axis[2] * np.sin(angle),
+            axis[0] * axis[2] * (1 - np.cos(angle)) + axis[1] * np.sin(angle)],
+
+            [axis[0] * axis[1] * (1 - np.cos(angle)) + axis[2] * np.sin(angle),
+            np.cos(angle) + axis[1] * axis[1] * (1 - np.cos(angle)),
+            axis[1] * axis[2] * (1 - np.cos(angle)) - axis[0] * np.sin(angle)],
+
+            [axis[0] * axis[2] * (1 - np.cos(angle)) - axis[1] * np.sin(angle),
+            axis[1] * axis[2] * (1 - np.cos(angle)) + axis[0] * np.sin(angle),
+            np.cos(angle) + axis[2] * axis[2] * (1 - np.cos(angle))]]
         
     return np.matmul(rotY, points)
     
 def rotateZ(angle, points):
-    axis = rotateX(30, rotateY(20, [0, 0, 1]))
     angle = angle / 180 * np.pi
-    rotZ = [[np.cos(angle) + axis[0] ** 2 * (1 - np.cos(angle)),
-              axis[0] * axis[1] * (1 - np.cos(angle)) - axis[2] * np.sin(angle),
-              axis[0] * axis[2] * (1 - np.cos(angle)) + axis[1] * np.sin(angle)],
-     
-             [axis[0] * axis[1] * (1 - np.cos(angle)) + axis[2] * np.sin(angle),
-              np.cos(angle) + axis[1] ** 2 * (1 - np.cos(angle)),
-              axis[1] * axis[2] * (1 - np.cos(angle)) - axis[1] * np.sin(angle)],
-     
-             [axis[0] * axis[2] * (1 - np.cos(angle)) - axis[1] * np.sin(angle),
-              axis[1] * axis[2] * (1 - np.cos(angle)) + axis[0] * np.sin(angle),
-              np.cos(angle) + axis[2] ** 2 * (1 - np.cos(angle))]]
+    axis = [0.34202014, -0.46984631, 0.81379768]
+    rotZ = [[np.cos(angle) + axis[0] * axis[0] * (1 - np.cos(angle)),
+            axis[0] * axis[1] * (1 - np.cos(angle)) - axis[2] * np.sin(angle),
+            axis[0] * axis[2] * (1 - np.cos(angle)) + axis[1] * np.sin(angle)],
+
+            [axis[0] * axis[1] * (1 - np.cos(angle)) + axis[2] * np.sin(angle),
+            np.cos(angle) + axis[1] * axis[1] * (1 - np.cos(angle)),
+            axis[1] * axis[2] * (1 - np.cos(angle)) - axis[0] * np.sin(angle)],
+
+            [axis[0] * axis[2] * (1 - np.cos(angle)) - axis[1] * np.sin(angle),
+            axis[1] * axis[2] * (1 - np.cos(angle)) + axis[0] * np.sin(angle),
+            np.cos(angle) + axis[2] * axis[2] * (1 - np.cos(angle))]]
         
     return np.matmul(rotZ, points)
         
@@ -48,7 +66,7 @@ class Display():
         pg.display.set_icon(image)
         pg.display.set_caption("Cube")
 
-        # Centre co-ordinates
+        # Cube centre co-ordinates
         self.x, self.y  = self.width / 2, self.height / 2
 
         # 3D Matrix of all verticies in a cube
@@ -61,38 +79,70 @@ class Display():
 
         self.yPhase = 0
         self.xPhase = 0
+        self.zPhase = 0
 
     def draw_cube(self):
-        points = self.cubePoints
+        points = rotateX(self.xPhase, (rotateY(self.yPhase, (rotateZ(self.zPhase, self.cubePoints)))))
+        for i in range(8):
+            points[0][i] += self.x
+            points[1][i] += self.y
+
+
         self.screen.fill((255, 255, 255))
             
         for i in range(8):
-            pg.draw.circle(self.screen, (150 + -0.25 * points[2][i], 100, 100), (self.x + points[0][i], self.y + points[1][i]), 4)
+            pg.draw.circle(self.screen, (150 + -0.25 * points[2][i], 100, 100), (points[0][i], points[1][i]), 4)
 
 
-        for i in range(4):
+        for i in range(1):
             pg.draw.line(self.screen,
                          (100, 100, 100),
-                         (self.x + points[0][i], self.y + points[1][i]),
-                         (self.x + points[0][4 + i], self.y + points[1][i + 4]))
+                         (points[0][i], points[1][i]),
+                         (points[0][4 + i], points[1][i + 4]))
             
             pg.draw.line(self.screen,
                          (100, 100, 100),
-                         (self.x + points[0][i], self.y + points[1][i]),
-                         (self.x + points[0][(i + 1) % 4], self.y + points[1][(i + 1) % 4]))
+                         (points[0][i], points[1][i]),
+                         (points[0][(i + 1) % 4], points[1][(i + 1) % 4]))
             
             pg.draw.line(self.screen,
                          (100, 100, 100),
-                         (self.x + points[0][i + 4], self.y + points[1][i + 4]),
-                         (self.x + points[0][(i + 1) % 4 + 4], self.y + points[1][(i + 1) % 4 + 4]))
+                         (points[0][i + 4], points[1][i + 4]),
+                         (points[0][(i + 1) % 4 + 4], points[1][(i + 1) % 4 + 4]))
+            
+        shade1 = 150
+        shade2 = 125
+        shade3 = 100
+        pg.draw.polygon(self.screen,
+                        (shade1, shade1, shade1),
+                        ((points[0][0], points[1][0]),
+                         (points[0][4], points[1][4]),
+                         (points[0][5], points[1][5]),
+                         (points[0][1], points[1][1])))
+        
+        pg.draw.polygon(self.screen, 
+                        (shade3, shade3, shade3),
+                        ((points[0][2], points[1][2]),
+                         (points[0][6], points[1][6]),
+                         (points[0][7], points[1][7]),
+                         (points[0][3], points[1][3])))
+        
+        pg.draw.polygon(self.screen,
+                        (shade2, shade2, shade2),
+                        ((points[0][1], points[1][1]),
+                         (points[0][5], points[1][5]),
+                         (points[0][6], points[1][6]),
+                         (points[0][2], points[1][2])))
             
 
-        self.cubePoints = rotateZ(1, points)
-        #if self.yPhase < 0: self.yPhase += 1
-        #if self.yPhase > 0: self.yPhase -= 1
-        #if self.xPhase < 0: self.xPhase += 1
-        #if self.xPhase > 0: self.xPhase -= 1
-        self.xPhase += 1
+        if self.yPhase < 0: self.yPhase += 1
+        if self.yPhase > 0: self.yPhase -= 1
+
+        if self.xPhase < 0: self.xPhase += 1
+        if self.xPhase > 0: self.xPhase -= 1
+
+        if self.zPhase < 0: self.zPhase += 1
+        if self.zPhase > 0: self.zPhase -= 1
 
 def main():
     screen = Display(700, 700)
@@ -106,15 +156,16 @@ def main():
 
         if keyDown:
             if key == pg.K_ESCAPE: running = False
-            #if key == pg.K_SPACE: screen.screen.fill((255, 255, 255))
+            if key == pg.K_SPACE: print(pg.key.get_pressed())
             if key == pg.K_RIGHT and screen.yPhase == 0: screen.yPhase = 90
             if key == pg.K_LEFT and screen.yPhase == 0: screen.yPhase = -90
-            if key == pg.K_DOWN and screen.xPhase == 0: screen.xPhase = -90
             if key == pg.K_UP and screen.xPhase == 0: screen.xPhase = 90
+            if key == pg.K_DOWN and screen.xPhase == 0: screen.xPhase = -90
+            if key == pg.K_z and screen.zPhase == 0: screen.zPhase = -90
 
         screen.draw_cube()
         pg.display.flip()
-        pg.time.wait(3)
+        pg.time.wait(5)
     pg.quit()
     
 main()
--- test.py
+++ test.py
@@ -1,43 +1,103 @@
-import numpy as np
+import pygame
+import os
+import math
+def matrix_multiplication(a, b):
+    columns_a = len(a[0])
+    rows_a = len(a)
+    columns_b = len(b[0])
+    rows_b = len(b)
 
-def rotateX(angle, points):
-    angle = angle / 180 * np.pi
-    rotX = [[1, 0, 0],
-            [0, np.cos(angle), -np.sin(angle)],
-            [0, np.sin(angle), np.cos(angle)]]
-        
-    return np.matmul(rotX, points)
-        
-def rotateY(angle, points):
-    angle = angle / 180 * np.pi
-    rotY = [[np.cos(angle), 0, np.sin(angle)],
-            [0, 1, 0],
-            [-np.sin(angle), 0, np.cos(angle)]]
-        
-    return np.matmul(rotY, points)
-    
-def rotateZ(angle, points):
-    angle = angle / 180 * np.pi
-    rotZ = [[np.cos(angle), -np.sin(angle), 0],
-            [np.sin(angle), np.cos(angle), 0],
-            [0, 0, 1]]
-        
-    return np.matmul(rotZ, points)
+    result_matrix = [[j for j in range(columns_b)] for i in range(rows_a)]
+    if columns_a == rows_b:
+        for x in range(rows_a):
+            for y in range(columns_b):
+                sum = 0
+                for k in range(columns_a):
+                    sum += a[x][k] * b[k][y]
+                result_matrix[x][y] = sum
+        return result_matrix
 
-axis = rotateX(30, rotateY(20, [0, 0, 1]))
+    else:
+        print("columns of the first matrix must be equal to the rows of the second matrix")
+        return None
 
-angle = np.pi / 2
+os.environ["SDL_VIDEO_CENTERED"]='1'
+black, white, blue  = (20, 20, 20), (230, 230, 230), (0, 154, 255)
+width, height = 1920, 1080
 
-r = [[np.cos(angle) + axis[0] ** 2 * (1 - np.cos(angle)),
-      axis[0] * axis[1] * (1 - np.cos(angle)) - axis[2] * np.sin(angle),
-      axis[0] * axis[2] * (1 - np.cos(angle)) + axis[1] * np.sin(angle)],
-     
-     [axis[0] * axis[1] * (1 - np.cos(angle)) + axis[2] * np.sin(angle),
-      np.cos(angle) + axis[1] ** 2 * (1 - np.cos(angle)),
-      axis[1] * axis[2] * (1 - np.cos(angle)) - axis[1] * np.sin(angle)],
-     
-     [axis[0] * axis[2] * (1 - np.cos(angle)) - axis[1] * np.sin(angle),
-      axis[1] * axis[2] * (1 - np.cos(angle)) + axis[0] * np.sin(angle),
-      np.cos(angle) + axis[2] ** 2 * (1 - np.cos(angle))]]
+pygame.init()
+pygame.display.set_caption("3D cube Projection")
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+fps = 60
 
+angle = 0
+cube_position = [width//2, height//2]
+scale = 600
+speed = 0.01
+points = [n for n in range(8)]
+
+points[0] = [[-1], [-1], [1]]
+points[1] = [[1], [-1], [1]]
+points[2] = [[1], [1], [1]]
+points[3] = [[-1], [1], [1]]
+points[4] = [[-1], [-1], [-1]]
+points[5] = [[1], [-1], [-1]]
+points[6] = [[1], [1], [-1]]
+points[7] = [[-1], [1], [-1]]
+
+
+def connect_point(i, j, k):
+    a = k[i]
+    b = k[j]
+    pygame.draw.line(screen, black, (a[0], a[1]), (b[0], b[1]), 2)
+
+run = True
+while run:
+    clock.tick(fps)
+    screen.fill(white)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    index = 0
+    projected_points = [j for j in range(len(points))]
+
+    rotation_x = [[1, 0, 0],
+                  [0, math.cos(angle), -math.sin(angle)],
+                  [0, math.sin(angle), math.cos(angle)]]
+
+    rotation_y = [[math.cos(angle), 0, -math.sin(angle)],
+                  [0, 1, 0],
+                  [math.sin(angle), 0, math.cos(angle)]]
+
+    rotation_z = [[math.cos(angle), -math.sin(angle), 0],
+                  [math.sin(angle), math.cos(angle), 0],
+                  [0, 0 ,1]]
+
+    for point in points:
+        rotated_2d = matrix_multiplication(rotation_y, point)
+        rotated_2d = matrix_multiplication(rotation_x, rotated_2d)
+        rotated_2d = matrix_multiplication(rotation_z, rotated_2d)
+        distance = 5
+        z = 1/(distance - rotated_2d[2][0])
+        projection_matrix = [[z, 0, 0],
+                            [0, z, 0]]
+        projected_2d = matrix_multiplication(projection_matrix, rotated_2d)
+
+        x = int(projected_2d[0][0] * scale) + cube_position[0]
+        y = int(projected_2d[1][0] * scale) + cube_position[1]
+        projected_points[index] = [x, y]
+        pygame.draw.circle(screen, blue, (x, y), 10)
+        index += 1
+    #draw edges
+    for m in range(4):
+        connect_point(m, (m+1)%4, projected_points)
+        connect_point(m+4, (m+1)%4 + 4, projected_points)
+        connect_point(m, m+4, projected_points)
+
+    angle += speed
+    pygame.display.update()
+
+pygame.quit()
     
