d1531f51b32faaf6d84b5ab8430ba1d121d50ce9
Callum Askew <18askewc@staidans.co.uk>
2024-07-03 14:20:44 +0100

    started the model for 3 by 3

--- Display/model_3.py
+++ Display/model_3.py
@@ -1,1 +1,12 @@
+import numpy as np
+
 class Model_3():
+    def __init__(self):
+        self.points = np.array([
+            ### Top Cubies
+                [  # Top Left
+                    [-1/3, -1  , -1   , -1/3, ],
+                    [-1  , -1  , -1   , -1  , ],
+                    [ 1/3, -1/3, -1   , -1  , ]
+                ]
+            ])
