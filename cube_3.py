1fa00790e2d1796ec066311b36db042c0f6d2523
Callum Askew <18askewc@staidans.co.uk>
2024-07-03 09:46:43 +0100

    I DIS SOME AUFF

--- cube_3.py
+++ cube_3.py
@@ -26,7 +26,19 @@ class Cube_3():
     def __getitem__(self, index):
         return self.cube[index]
     
+    def X(self):
+        # Returns rotation around X axis clockwise
+        return [self[2],
+                
+                self[1][2] + self[1][5] + self[1][8] + self[1][1] + self[1][4] + self[1][7] + self[1][0] + self[1][3] + self[1][6],
+                self[5],
+                self[3][6] + self[3][3] + self[3][0] + self[3][7] + self[3][4] + self[3][1] + self[3][8] + self[3][5] + self[3][2],
+                self[0][::-1],
+                
+                self[4][::-1]]
+    
     def X_Prime(self):
+        # Returns rotation around X axis anticlockwise
         return [self[4][::-1],
                 
                 self[1][6] + self[1][3] + self[1][0] + self[1][7] + self[1][4] + self[1][1] + self[1][8] + self[1][5] + self[1][2],
@@ -36,6 +48,32 @@ class Cube_3():
                 
                 self[2]]
 
+    
+    def Y(self):
+        # Return rotation around Y axis clockwise
+        return [self[0][6] + self[0][3] + self[0][0] + self[0][7] + self[0][4] + self[0][1] + self[0][8] + self[0][5] + self[0][2],
+                
+                self[2],
+                self[3],
+                self[4],
+                self[1],
+                
+                self[5][2] + self[5][5] + self[5][8] + self[5][1] + self[5][4] + self[5][7] + self[5][0] + self[5][3] + self[5][6]]
+    
+    def Y_Prime(self):
+        # Return rotation around Y axis anticlockwise
+        return [self[0][2] + self[0][5] + self[0][8] + self[0][1] + self[0][4] + self[0][7] + self[0][0] + self[0][3] + self[0][6],
+                
+                self[4],
+                self[1],
+                self[2],
+                self[3],
+                
+                self[5][6] + self[5][3] + self[5][0] + self[5][7] + self[5][4] + self[5][1] + self[5][8] + self[5][5] + self[5][2]]
+
+
+    def Z(self):
+        # Return rotation around Z axis clockwise
 # cube = Cube_3()
 cube = Cube_3(["OWROWOBRB",
                
@@ -46,5 +84,6 @@ cube = Cube_3(["OWROWOBRB",
 
                "GOGYYOYWW"])
 
-cube.cube = cube.X_Prime()
+cube.cube = cube.Y_Prime()
+
 cube.display()
