fb936528df687f050b1317749a3a683afc10d6eb
Callum Askew <18askewc@staidans.co.uk>
2024-09-30 10:14:59 +0100

    WHERES THE 16

--- phase_3_table_read.py
+++ phase_3_table_read.py
@@ -1,18 +1,21 @@
+from ast import Try
 from cube_3 import Cube3
+import random as rnd
 
 MOVE_KEYS = ("L", "L2", "L'", "F2", "R", "R2", "R'", "B2", "U2", "D2")
 
 
 
-def read_table(table_index, line_index):
-    moves_1, moves_2 = PAGE_FUNCS[table_index - 1](line_index)
+def read_table(page_num, line_index):
+    moves_1, moves_2 = read_line(page_num, line_index)
+    
     for i, move in enumerate(moves_1):
         try: moves_1[i] = MOVE_KEYS[int(move) - 1]
-        except: pass
+        except: print("UNRECOGNISED MOVE")
             
     for i, move in enumerate(moves_2):
         try: moves_2[i] = MOVE_KEYS[int(move) - 1]
-        except: pass
+        except: print("UNRECOGNISED MOVE")
             
     return moves_1, moves_2
 
@@ -24,27 +27,31 @@ def read_page_1(line_index):
         moves2 = line[47:].replace("  ", " ").split(" ")
         return moves1, moves2
         
-            
 
-                
-
-def read_page_2(line_index):
-    with open ("phase_3_page_2.txt", "r") as table:
+def read_line(page_num, line_index):
+    with open(f"phase_3_page_{page_num}.txt", "r") as table:
         line = table.readlines()[line_index]
-        moves1 = line[11:45].strip("   ").replace("  ", " ").split(" ")
-        print(moves_1)
         
+        # Split string into 2 movesets
+        line = line[4:].split("   ")
+        i = 0
+        while i < len(line):
+            if line[i] == "": line.pop(i)
+            else: i += 1
 
-def read_line():
+        moves_1 = line[0].strip("\n").strip(" ").replace("  ", " ").split(" ")
+        try: moves_2 = line[1].strip("\n").strip(" ").replace("  ", " ").split(" ")
+        except IndexError: moves_2 = []
+        print(line)     
+                
+    return moves_1, moves_2
 
-PAGE_FUNCS = (read_page_1, read_page_2)
 
 
-for i in range(70):
-    cube = Cube3(["cccc-cccc", "---------", "aaaa-aaaa", "---------", "aaaa-aaaa", "cccc-cccc"])
-    moves_1, moves_2 = read_table(1, 3)
-    for move in moves_1:
-        print(move, end = ", ")
-        cube.move(move)
 
-    cube.display()
+cube =  Cube3(["111111111", "---------", "000000000", "---------", "000000000", "111111111"])
+moves_1, moves_2 = read_table(7, rnd.randint(0, 69))
+for move in moves_1:
+    cube.move(move)
+
+cube.display()
