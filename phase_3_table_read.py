4556d73d5ce25d75f8b01776262e40dba881a651
Callum Askew <18askewc@staidans.co.uk>
2024-09-30 11:31:51 +0100

    I tihkn its working

--- phase_3_table_read.py
+++ phase_3_table_read.py
@@ -11,11 +11,15 @@ def read_table(page_num, line_index):
     
     for i, move in enumerate(moves_1):
         try: moves_1[i] = MOVE_KEYS[int(move) - 1]
-        except: print("UNRECOGNISED MOVE")
+        except: 
+            print("UNRECOGNISED MOVE", page_num, line_index, 1)
+            raise Exception
             
     for i, move in enumerate(moves_2):
         try: moves_2[i] = MOVE_KEYS[int(move) - 1]
-        except: print("UNRECOGNISED MOVE")
+        except:
+            print("UNRECOGNISED MOVE", page_num, line_index, 1)
+            raise Exception
             
     return moves_1, moves_2
 
@@ -39,19 +43,22 @@ def read_line(page_num, line_index):
             if line[i] == "": line.pop(i)
             else: i += 1
 
-        moves_1 = line[0].strip("\n").strip(" ").replace("  ", " ").split(" ")
+        if line[0] == "-": moves_1 = []
+        else: moves_1 = line[0].strip("\n").strip(" ").replace("  ", " ").split(" ")
         try: moves_2 = line[1].strip("\n").strip(" ").replace("  ", " ").split(" ")
         except IndexError: moves_2 = []
-        print(line)     
+        print(line)      
                 
     return moves_1, moves_2
 
 
 
 
-cube =  Cube3(["111111111", "---------", "000000000", "---------", "000000000", "111111111"])
-moves_1, moves_2 = read_table(7, rnd.randint(0, 69))
-for move in moves_1:
-    cube.move(move)
+for i in range(7):
+    for j in range(69):
+        cube =  Cube3(["111111111", "---------", "000000000", "---------", "000000000", "111111111"])
+        moves_1, moves_2 = read_table(i + 1, j)
+        for move in moves_2:
+            cube.move(move)
 
-cube.display()
+        cube.display()
