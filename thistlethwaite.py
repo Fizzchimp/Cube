52149b0bb340f756182cfa4fb74c354d237d5168
Callum Askew <18askewc@staidans.co.uk>
2024-10-23 15:25:25 +0100

    updated i dont know whats oing on

--- thistlethwaite.py
+++ thistlethwaite.py
@@ -282,7 +282,7 @@ def phase_2(G_1_state):
 ORBIT_MOVES = {
     "00000000" : [],
     "10100000" : [],
-    "10000010" : [],
+#    "10000010" : [],
     "11001100" : [],
     "10000100" : [6],
     "11000000" : [2, 8],
@@ -744,43 +744,34 @@ def phase_4(cube):
 
     
 # Function to organise solving the cube
-def thistle_solve(start_state):
-    Cube3(start_state).display()
+def thistle_solve(start_cube):
+    print(start_cube.cube)
     # Phase 1
     timer = time.time()
-    phase_1_moves, G_1_state = phase_1(start_state) 
+    phase_1_moves, G_1_state = phase_1(start_cube) 
     print(f"Phase 1 finished in {time.time() - timer}s")
     print(phase_1_moves)
-    Cube3(G_1_state).display()
+    print(G_1_state)
     
 
     # Phase 2
     timer = time.time()
-    phase_2_moves, G_2_state = phase_2(G_1_state)
+    phase_2_moves, G_2_node = phase_2(G_1_state)
     print(f"Phase 2 finished in {time.time() - timer}s")
     print(phase_2_moves)
-    Cube3(G_2_state).display()
+    print(G_2_node.cube)
 
 
 
     timer = time.time()
-    phase_3_moves, G_3_cube = phase_3(G_2_state)
+    phase_3_moves, G_3_cube = phase_3(G_2_node)
     print(f"Phase 3 finished in {time.time() - timer}s")
     print("Phase 3 cube:")
-    G_3_cube.display()
+    print(G_3_cube.cube)
 
     phase_4_moves = phase_4(G_3_cube)
     print("Phase 4 moves:", phase_4_moves)
     return phase_1_moves + phase_2_moves + phase_3_moves + phase_4_moves
 
-cube = Cube3()
-for i in range(1):
-    print("==========\nSTARTING\n==========")
-    cube.scramble()
-    print(cube.cube)
-    try:
-        print(thistle_solve(cube))
-        break
-    except: print("Error")
-    
-print(i)
+cube = Cube3(['GWBYWOWOW', 'OGBBGGGBB', 'OYGRROOOY', 'RBWGBWORR', 'RRWGOWBBY', 'YWGRYYRYY'])
+print(thistle_solve(cube))
