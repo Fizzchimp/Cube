ee6630fe782d55ea4e15258df3275a029bbf4df7
Fizzchimp <callumaskew@gmail.com>
2024-12-12 15:07:32 +0000

    updated

--- Thistlethwaite/thistlethwaite.py
+++ Thistlethwaite/thistlethwaite.py
@@ -5,7 +5,7 @@ import random
 from Thistlethwaite.phase_1 import phase_1
 from Thistlethwaite.phase_2 import phase_2
 from Thistlethwaite.phase_3 import phase_3
-from Thistlethwaite.phase_4 import phase_4, bfs_sides
+from Thistlethwaite.phase_4 import phase_4
 
 #from cube_3 import Cube3
 from Assets.node_3 import Node
