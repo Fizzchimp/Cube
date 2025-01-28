b81ab4782f61fa9302f4fde42b84d03bcf5b1b46
Callum Askew <18askewc@staidans.co.uk>
2025-01-28 15:27:08 +0000

    Updated solving agorithm

--- Assets/solve_2.py
+++ Assets/solve_2.py
@@ -24,14 +24,14 @@ def solve_2(startState):
     sNodeQ = Queue(99999)
 
     # Current start node
-    cSNode = Node(startState)
+    cSNode = Node(startState, None, "-")
     
 
     # Queue for current end nodes
     eNodeQ = Queue(99999)
 
     # Current end node
-    cENode = Node(normalisedSolved(startState))
+    cENode = Node(normalisedSolved(startState), None, "-")
     
     # Used to determine when all nodes in one depth have been explored
     generation = 0
@@ -40,7 +40,7 @@ def solve_2(startState):
     vENodes = []
     vSNodes = []
     
-    while generation <= 6:
+    while generation <= 5:
         ## Start state tree
         vSNodes = []
         nextGen = generation + 1
@@ -56,20 +56,20 @@ def solve_2(startState):
             vSNodes.append(cSNode)
 
             # Enqueue all adjacent nodes
-            if cSNode.movement != "U'":  
+            if cSNode.movement[0] != "U":  
                 sNodeQ.enqueue(Node(cSNode.U(), cSNode, "U", nextGen))
-            if cSNode.movement != "U":   
                 sNodeQ.enqueue(Node(cSNode.U_Prime(), cSNode, "U'", nextGen))
+                sNodeQ.enqueue(Node(cSNode.U_2(), cSNode, "U2", nextGen))
 
-            if cSNode.movement != "R'":  
-                sNodeQ.enqueue(Node(cSNode.R(), cSNode, "R", nextGen))
-            if cSNode.movement != "R":   
+            if cSNode.movement[0] != "R":  
+                sNodeQ.enqueue(Node(cSNode.R(), cSNode, "R", nextGen))  
                 sNodeQ.enqueue(Node(cSNode.R_Prime(), cSNode, "R'", nextGen))
+                sNodeQ.enqueue(Node(cSNode.R_2(), cSNode, "R2", nextGen))
             
-            if cSNode.movement != "F'":  
-                sNodeQ.enqueue(Node(cSNode.F(), cSNode, "F", nextGen))
-            if cSNode.movement != "F":   
+            if cSNode.movement[0] != "F":  
+                sNodeQ.enqueue(Node(cSNode.F(), cSNode, "F", nextGen))  
                 sNodeQ.enqueue(Node(cSNode.F_Prime(), cSNode, "F'", nextGen))
+                sNodeQ.enqueue(Node(cSNode.F_2(), cSNode, "F2", nextGen))
 
             # Fetch the next node
             cSNode = sNodeQ.dequeue()
@@ -90,20 +90,20 @@ def solve_2(startState):
             vENodes.append(cENode)
 
             # Enqueue all adjacent nodes
-            if cENode.movement != "U'":
-                eNodeQ.enqueue(Node(cENode.U_Prime(), cENode, "U", nextGen))
-            if cENode.movement != "U":   
+            if cENode.movement[0] != "U":
+                eNodeQ.enqueue(Node(cENode.U_Prime(), cENode, "U", nextGen)) 
                 eNodeQ.enqueue(Node(cENode.U(), cENode, "U'", nextGen))
+                eNodeQ.enqueue(Node(cENode.U_2(), cENode, "U2", nextGen))
 
-            if cENode.movement != "R'":  
-                eNodeQ.enqueue(Node(cENode.R_Prime(), cENode, "R", nextGen))
-            if cENode.movement != "R":   
+            if cENode.movement[0] != "R":  
+                eNodeQ.enqueue(Node(cENode.R_Prime(), cENode, "R", nextGen)) 
                 eNodeQ.enqueue(Node(cENode.R(), cENode, "R'", nextGen))
+                eNodeQ.enqueue(Node(cENode.R_2(), cENode, "R2", nextGen))
             
-            if cENode.movement != "F'":  
+            if cENode.movement[0] != "F":  
                 eNodeQ.enqueue(Node(cENode.F_Prime(), cENode, "F", nextGen))
-            if cENode.movement != "F":   
                 eNodeQ.enqueue(Node(cENode.F(), cENode, "F'", nextGen))
+                eNodeQ.enqueue(Node(cENode.F_2(), cENode, "F2", nextGen))
 
             # Fetch the next end node
             cENode = eNodeQ.dequeue()
@@ -118,7 +118,7 @@ def solve_2(startState):
 
 
 
-    ### Final generation 7 check
+    ### Final generation 6 check
 
     # Start tree
     vSNodes = []
